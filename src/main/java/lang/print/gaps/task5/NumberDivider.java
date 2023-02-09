8e95fd55e378400ed1607a53f0c70eb825dd7297
kseniakaratchenia <62858256+kseniakaratchenia@users.noreply.github.com>
2023-02-09 11:12:22 -0800

    operators

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        int division = 5;
+        int result = toBeDivided / division;
+        System.out.println(result);
     }
 }
